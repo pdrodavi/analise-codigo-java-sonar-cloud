463c2a4c6540a6a08115dd22f63d6fe7cd083414
Pedro Davi <pdrodavi@outlook.com>
2021-09-29 22:17:57 -0300

    mais testes

--- src/test/java/br/com/pedrodavi/analisesonarcloud/model/AddressResponseDTOTest.java
+++ src/test/java/br/com/pedrodavi/analisesonarcloud/model/AddressResponseDTOTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static com.google.code.beanmatchers.BeanMatchers.*;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AddressResponseDTOTest {
 
@@ -17,6 +18,18 @@ class AddressResponseDTOTest {
                 hasValidBeanEquals(),
                 hasValidBeanToString()
         ));
+    }
+
+    @Test
+    void builderConstruct() {
+        AddressResponseDTO addressResponseDTO = new AddressResponseDTO("", "", "", "", "", "", "", "", "", "");
+        AddressResponseDTO addressResponseDTOBuilder = AddressResponseDTO.builder()
+                .bairro("").cep("").complemento("").ddd("").gia("")
+                .ibge("").localidade("").logradouro("").siafi("")
+                .uf("").build();
+
+        assertNotNull(addressResponseDTO);
+        assertNotNull(addressResponseDTOBuilder);
     }
 
 }
